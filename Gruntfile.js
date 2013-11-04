98fe6f5ef733ae31e93a279b90516960eb63f081
Sahat Yalkabov <sakhat@gmail.com>
2013-11-03 22:16:21 -0500

    Gruntfile outputs one single file with built-in almond.js, and global variable is exposed as "skeleton"

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,27 +3,37 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     requirejs: {
-      options: {
-        mainConfigFile: "app/main.js",
-        include: ["main"],
-        insertRequire: ["main"],
-        out: "dist/source.min.js",
-        optimize: 'none',
-        name: "almond",
-        baseUrl: "dist/app",
-        wrap: true,
-        preserveLicenseComments: false
+      compile: {
+        options: {
+          baseUrl: "src",
+          name: '../vendor/almond',
+          optimize: 'none',
+          include: ['skeleton'],
+          wrap: {
+            startFile: 'src/start.frag',
+            endFile: 'src/end.frag'
+          },
+          // build file destination, relative to the build file itself
+          out: "dist/skeleton.js",
+          preserveLicenseComments: false
+        }
       }
     },
 
-    min: {
-      "dist/skeleton.min.js": "dist/skeleton.js"
+    uglify: {
+      options: {
+        files: {
+          "dist/skeleton.min.js": "dist/skeleton.js"
+        }
+      }
     }
+
   });
 
   // Grunt tasks.
   grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // When running the default Grunt command, just lint the code.
-  grunt.registerTask('default', ['requirejs', 'min']);
+  grunt.registerTask('default', ['requirejs', 'uglify']);
 };

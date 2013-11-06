496fd01aa559984d56c15c29e05e7c36b22c19e9
Sahat Yalkabov <sakhat@gmail.com>
2013-11-06 17:26:26 -0500

    Added karma support with require.js

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ requirejs-library
 =================
 
 skeleton project for building javascript libraries using requirejs
+
+Simply run `grunt` to buils the library
---
+++ test/test-main.js
@@ -0,0 +1,17 @@
+var tests = [];
+for (var file in window.__karma__.files) {
+  console.log(file);
+  if (/spec\//.test(file)) {
+    tests.push(file);
+  }
+}
+
+requirejs.config({
+  baseUrl: '/base/src',
+  paths: {
+    lodash: '../bower_components/lodash/dist/lodash.min',
+    spec: '../test/spec'
+  },
+  deps: tests,
+  callback: window.__karma__.start
+});

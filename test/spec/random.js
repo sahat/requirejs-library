613da87e530b5b8177ef39bf07abe9f5074c8972
Sahat Yalkabov <sakhat@gmail.com>
2013-11-10 23:26:42 -0500

    Cleaned up HTML spec runners Added random.js spec

---
+++ test/spec/random.js
@@ -0,0 +1,25 @@
+define(['random'], function(random) {
+
+  describe('Random Utility', function() {
+    var min = Math.pow(2, 31);
+    var max = Math.pow(2, 62);
+
+    // Custom Jasmine Matcher
+    beforeEach(function() {
+      this.addMatchers({
+        toBeGreaterThanOrEqualTo: function(expected) {
+          return this.actual >= expected;
+        }
+      });
+    });
+
+    it('should produce a random number greater than or equal to the minimum number', function() {
+      expect(random(min, max)).toBeGreaterThanOrEqualTo(min);
+    });
+
+    it('should produce a random number when passed Number.MAX_VALUE', function() {
+      expect(random(Number.MAX_VALUE)).toBeGreaterThan(0);
+    });
+  });
+
+});

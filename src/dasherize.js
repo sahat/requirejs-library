b7c49862d40f5031b73853da1766975f5578fca7
Sahat Yalkabov <sakhat@gmail.com>
2013-11-10 22:45:28 -0500

    Fixed a typo in dasherize.js

--- src/dasherize.js
+++ src/dasherize.js
@@ -1,7 +1,7 @@
 define(['trim'], function(trim){
 
   function dasherize(str){
-    return _s.trim(str).replace(/([A-Z])/g, '-$1').replace(/[-_\s]+/g, '-').toLowerCase();
+    return trim(str).replace(/([A-Z])/g, '-$1').replace(/[-_\s]+/g, '-').toLowerCase();
   }
 
   return dasherize;

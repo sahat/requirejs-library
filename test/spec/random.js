473988526c75785dab506c9e1b1c3bcbc0b44298
Sahat Yalkabov <sakhat@gmail.com>
2014-01-21 08:45:46 -0500

    Failing test

--- test/spec/random.js
+++ test/spec/random.js
@@ -14,7 +14,7 @@ define(['random'], function(random) {
     });
 
     it('should produce a random number greater than or equal to the minimum number', function() {
-      expect(random(min, max)).toBeGreaterThanOrEqualTo(min);
+      expect(random(min, max)).toBeGreaterThanOrEqualTo(max);
     });
 
     it('should produce a random number when passed Number.MAX_VALUE', function() {
